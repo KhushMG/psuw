bc196dbfd6ba0b2f65a0f8d9d5596f6804f94471
KhushMG <khushh.meet@gmail.com>
2023-12-01 16:55:17 -0800

    begin

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,9 @@
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
 import './globals.css'
+import { GeistSans } from "geist/font/sans";
 
-const inter = Inter({ subsets: ['latin'] })
+
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
-  )
+  );
 }
