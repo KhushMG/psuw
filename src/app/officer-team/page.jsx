4aea3074225abb57b1d5f6c2ab1858026402527f
KhushMG <khushh.meet@gmail.com>
2023-12-06 18:48:46 -0800

    beginning grid for officer team

--- src/app/officer-team/page.jsx
+++ src/app/officer-team/page.jsx
@@ -13,6 +13,16 @@ export default function About() {
               {"Meet the Team!"}
             </h2>
           </div>
+          {/* grid container */}
+          <div class="gridcont">
+            <div className="grid-item">Item 1</div>
+            <div className="grid-item">Item 2</div>
+            <div className="grid-item">Item 3</div>
+            <div className="grid-item">Item 4</div>
+            <div className="grid-item">Item 5</div>
+            <div className="grid-item">Item 6</div>
+            <div className="grid-item">Item 7</div>
+          </div>
         </div>
       </div>
     </div>
