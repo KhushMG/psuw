da05517825a74af751160ca5f86a9ad9ab8d131a
KhushMG <khushh.meet@gmail.com>
2023-12-09 12:32:04 -0800

    Refactored Officer Cards

---
+++ src/app/components/OfficerCard.jsx
@@ -0,0 +1,25 @@
+"use client";
+import logo from "../logo.png";
+import Image from "next/image";
+
+// props: Officer photo, officer name 
+const OfficerCard = (props) => {
+    return (
+      <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
+        {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
+        <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
+          <Image
+            src={props.image}
+            alt="Officer 5"
+            className="fill-image rounded-md"
+          />
+        </div>
+        <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-7">
+          <h2 className="spec-font font-semibold md:text-2xl"> {props.name}</h2>
+        </div>
+        {/* </div> */}
+      </div>
+    );
+};
+
+export default OfficerCard; 
--- src/app/officer-team/page.jsx
+++ src/app/officer-team/page.jsx
@@ -7,6 +7,7 @@ import team4 from "../assets/4.png";
 import team5 from "../assets/5.png";
 import team6 from "../assets/6.png";
 import team7 from "../assets/7.png";
+import OfficerCard from "../components/OfficerCard";
 
 export default function About() {
   return (
@@ -21,24 +22,11 @@ export default function About() {
               {"Meet the Team!"}
             </h2>
           </div>
+          {/* Officer Cards. Props: Officer image, officer name. Reference OfficerCard.jsx for stylnig */}
           <div className="gridcont flex flex-col mt-10">
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team1}
-                  alt="Officer 1"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-5">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Marie Kang
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
+            <OfficerCard image={team1} name={"Marie Kang"} />
+
+            {/* dont touch this one */}
             <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
               {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
               <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
@@ -48,7 +36,7 @@ export default function About() {
                   className="fill-image rounded-md"
                 />
               </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-6 lg:mt-7">
+              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-6 lg:mt-10">
                 <h2 className="spec-font font-semibold md:text-2xl">
                   {" "}
                   Non Pronanun
@@ -56,91 +44,16 @@ export default function About() {
               </div>
               {/* </div> */}
             </div>
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team3}
-                  alt="Officer 3"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-5">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Thomas Lee
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team4}
-                  alt="Officer 4"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-7">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Chloe Chung
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team5}
-                  alt="Officer 5"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-7">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Jasper Gan
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team6}
-                  alt="Officer 6"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-7">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Sean Yuan
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
-            <div className="grid-item bg-purple-300/70 rounded-md p-[2rem] shadow-md">
-              {/* <div className="hover:opacity-0 transition duration-150 ease-in-out"> */}
-              <div className="image-border flex justify-center p-[2rem] border-[0.3rem] border-white rounded-md">
-                <Image
-                  src={team7}
-                  alt="Officer 7"
-                  className="fill-image rounded-md"
-                />
-              </div>
-              <div className="under-image-border flex justify-center items-center py-[1rem] border-[0.3rem] border-white rounded-md mt-6">
-                <h2 className="spec-font font-semibold md:text-2xl">
-                  {" "}
-                  Madelyn Lee
-                </h2>
-              </div>
-              {/* </div> */}
-            </div>
+
+            <OfficerCard image={team3} name={"Thomas Lee"} />
+
+            <OfficerCard image={team4} name={"Chloe Chung"} />
+
+            <OfficerCard image={team5} name={"Jasper Gan"} />
+
+            <OfficerCard image={team6} name={"Sean Yuan"} />
+
+            <OfficerCard image={team7} name={"Madelyn Lee"} />
           </div>
         </div>
       </div>
